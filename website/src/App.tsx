5517d5818c34b1c613a6a2d8cdde2c49ff3cbc7c
Filip Koed Nymann <87913095+FleshIsBack@users.noreply.github.com>
2023-10-13 11:05:38 +0200

    Added FileStructure and navigation

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from './components/nav/Nav';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+    <Nav />
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<div>About</div>} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
