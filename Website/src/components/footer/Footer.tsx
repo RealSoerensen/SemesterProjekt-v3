6c3e84d0da05f32debe9475623d1a9da881ee894
SvendBD <115786653+SvendBD@users.noreply.github.com>
2023-10-13 13:40:11 +0200

    Footer

--- Website/src/components/footer/Footer.tsx
+++ Website/src/components/footer/Footer.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faPhone} from '@fortawesome/free-solid-svg-icons'
 
-type Props = {}
+type Props = {};
 
 const Footer = (props: Props) => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const footerStyle: React.CSSProperties = {
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+    backgroundColor: '#333',
+    color: '#fff',
+    padding: '10px 0',
+    textAlign: 'center',
+  };
 
-export default Footer
+  return (
+    <footer style={footerStyle}>
+      <div className="container">
+        <p>&copy; {new Date().getFullYear()} Padel Shop</p>
+        <p><FontAwesomeIcon icon={faPhone} />  Contact Patrick, out lead developer: 60513224  <FontAwesomeIcon icon={faPhone} flip="horizontal"/></p> {/* Add your phone number here */}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
