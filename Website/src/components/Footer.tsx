ed17758dbf5cc5809ebb00d0e1166c5c4d3be97e
Filip Koed Nymann <87913095+FleshIsBack@users.noreply.github.com>
2023-10-18 14:48:51 +0200

    FooterMarginFixed

--- Website/src/components/Footer.tsx
+++ Website/src/components/Footer.tsx
@@ -7,9 +7,9 @@ const Footer = () => {
 
     return (
         <footer className='bg-dark text-center text-light'>
-            <div className="container mt-3">
-                <p>&copy; {new Date().getFullYear()} Padel Shop</p>
-                <p>
+            <div className="container mt-3 mb-0 pb-0">
+                <p className='p-0' >&copy; {new Date().getFullYear()} Padel Shop</p>
+                <p className='p-0 m-0'>
                     <FontAwesomeIcon icon={phoneIcon} />
                     Contact Patrick, our lead developer: <a href="tel:+4560513224">60513224</a>
                     <FontAwesomeIcon icon={phoneIcon} flip="horizontal" />
