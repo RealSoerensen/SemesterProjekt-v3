1b78253a9b7e365d25d979023779a3e30c75aa14
Patrick Sørensen <10461259@ucn.dk>
2023-10-18 12:34:03 +0200

    IT FUCKING WORKS

--- Models/ProductDescription.cs
+++ Models/ProductDescription.cs
@@ -6,13 +6,31 @@ public class ProductDescription
 {
     public long? Id { get; set; }
     public string Description { get; set; }
-    public byte[] Image { get; set; }
+    public string Image { get; set; }
     public decimal Price { get; set; }
     public string Name { get; set; }
     public long Stock { get; set; }
     public string Brand { get; set; }
 
-    ProductDescription()
+    [JsonConstructor]
+    public ProductDescription(string description, string image, decimal price, string name, long stock, string brand)
     {
+        Description = description;
+        Image = image;
+        Price = price;
+        Name = name;
+        Stock = stock;
+        Brand = brand;
+    }
+
+    public ProductDescription(long id, string description, string image, decimal price, string name, long stock, string brand)
+    {
+        Id = id;
+        Description = description;
+        Image = image;
+        Price = price;
+        Name = name;
+        Stock = stock;
+        Brand = brand;
     }
 }
--- Website/src/services/ProductDescription.tsx
+++ Website/src/services/ProductDescription.tsx
@@ -19,12 +19,28 @@ export async function getAllProductDescription() {
         return null;
     }
 }
+
 export async function getProductDescriptionById(id: number): Promise<ProductDescription | null> {
     try {
         const response = await axios.get(`${url}/${id}`);
 
         if (response.status === 200) {
-            return response.data;
+            // Assuming response.data.Image is the byte[] representing an image
+            const productDescription: ProductDescription = response.data;
+            const imageBlob = new Blob([productDescription.image], { type: 'image/jpeg' }); // Adjust the type as per your image format
+
+            const reader = new FileReader();
+            reader.readAsDataURL(imageBlob);
+
+            reader.onload = function () {
+                // Here, reader.result will be a data URL that can be set as the src of an <img> element
+                if (typeof reader.result === 'string')
+                    productDescription.image = reader.result; // You can add this new property to your ProductDescription class
+
+            };
+
+            return productDescription;
+
         } else {
             return null;
         }
