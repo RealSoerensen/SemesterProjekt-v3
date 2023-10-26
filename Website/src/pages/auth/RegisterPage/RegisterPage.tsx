9477c33d7d405883ebbd9478c87159c2ea2c2367
SvendBD <115786653+SvendBD@users.noreply.github.com>
2023-10-26 10:36:08 +0200

    Validations

--- Website/src/pages/auth/RegisterPage/RegisterPage.tsx
+++ Website/src/pages/auth/RegisterPage/RegisterPage.tsx
@@ -49,13 +49,58 @@ const RegisterPage = () => {
     const handleConfirm = () => {
         let newErrorMessage = '';
         for (let i = 0; i < formdata1.length; i++) {
-            if (formdata1[i].value === "") {
-                newErrorMessage += `${formdata1[i].placeholder} is required, `
+            const field = formdata1[i];
+            const value = field.value;
+            const placeholder = field.placeholder;
+    
+            // Check for empty fields
+            if (value === "") {
+                newErrorMessage += `${placeholder} is required, `;
+                setNumberOfErrors(numberOfErrors + 1);
+                continue;
             }
-
+    
+            // Additional validations
+            switch (field.name) {
+                case "FirstName":
+                case "LastName":
+                    if (value.length < 2 || value.length > 50) {
+                        newErrorMessage += `${placeholder} must be between 2 and 50 characters, `;
+                        setNumberOfErrors(numberOfErrors + 1);
+                    }
+                    break;
+                case "Email":
+                    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+                    if (!emailRegex.test(value)) {
+                        newErrorMessage += `Invalid email format, `;
+                        setNumberOfErrors(numberOfErrors + 1);
+                    }
+                    break;
+                case "Password":
+                    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+                    if (!passwordRegex.test(value)) {
+                        newErrorMessage += `Password must contain at least one uppercase letter, one lowercase letter, and one number, and be at least 8 characters long, `;
+                        setNumberOfErrors(numberOfErrors + 1);
+                    }
+                    break;
+                case "Street":
+                case "City":
+                case "Zip":
+                case "HouseNumber":
+                    if (value.length < 1 || value.length > 4) {
+                        newErrorMessage += `${placeholder} must be between 1 and 4 characters, `;
+                        setNumberOfErrors(numberOfErrors + 1);
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
-        setErrorMessage(newErrorMessage);
+        if(numberOfErrors > 0) {
+            setErrorMessage(newErrorMessage);
+        }
     };
+    
     return (
         <div className="container text-center ">
             <div className='row'>
