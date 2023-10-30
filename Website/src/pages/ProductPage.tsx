f923be8bb1e8b1c6d2636760bde149ed3af2084c
Patrick Sørensen <10461259@ucn.dk>
2023-10-30 09:17:15 +0100

    Added category to person

--- Website/src/pages/ProductPage.tsx
+++ Website/src/pages/ProductPage.tsx
@@ -1,13 +1,36 @@
 import { useParams } from "react-router-dom";
+import Product from "../models/Product";
+import { useEffect, useState } from "react";
+import { getProductById } from "../services/ProductService";
+
 
 const ProductPage = () => {
     const { id } = useParams<{ id: string }>();
+    const [product, setProduct] = useState<Product | null>(null);
+
+    useEffect(() => {
+        if (id) {
+            getProductById(parseInt(id)).then((product) => setProduct(product));
+        }
+    }, [id]);
+
+
 
     return (
         <div>
             Produkt side
             <br />
-            id: {id}
+            Product id: {id}
+            <br />
+            Produkt navn: {product?.name}
+            <br />
+            Produkt beskrivelse: {product?.description}
+            <br />
+            Produkt billede: {product?.image}
+            <br />
+            Produkt kategori: {product?.category}
+            <br />
+            Produkt brand: {product?.brand}
         </div>
     )
 }
