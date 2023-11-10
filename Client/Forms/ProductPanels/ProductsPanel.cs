cdbd8d7222c4ff0b24786cbf3045bbde468dd586
Patrick Sørensen <10461259@ucn.dk>
2023-11-10 10:13:47 +0100

    Product in datagridview

--- Client/Forms/ProductPanels/ProductsPanel.cs
+++ Client/Forms/ProductPanels/ProductsPanel.cs
@@ -1,21 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using Client.Controllers;
+using Models;
 
-namespace Client {
-    public partial class ProductsPanel : Form {
-        public ProductsPanel() {
-            InitializeComponent();
+namespace Client.Forms.ProductPanels;
+
+public partial class ProductsPanel : Form
+{
+    private readonly ProductController productController = new();
+    private List<Product> products = new();
+
+    public ProductsPanel()
+    {
+        InitializeComponent();
+    }
+
+    private void ProductsPanel_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            products = productController.GetAll();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(@"Kunne ikke hente produkter");
+            Console.WriteLine(ex);
+            Close();
         }
 
-        private void trackBar1_Scroll(object sender, EventArgs e) {
+        productGrid.DataSource = products;
+    }
 
-        }
+    private void productGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+    {
+        var product = products[e.RowIndex];
+        var editProduct = new EditProduct(product);
+        editProduct.ShowDialog();
+        if (editProduct.DialogResult != DialogResult.OK) return;
+        productController.Update(editProduct.Product);
+        var index = products.FindIndex(p => p.ID == product.ID);
+        products[index] = product;
+        productGrid.DataSource = products;
+    }
+
+    private void btnCreateProduct_Click(object sender, EventArgs e)
+    {
+        return;
     }
 }
