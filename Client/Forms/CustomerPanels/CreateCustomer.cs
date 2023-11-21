9fe46ef15a04342e5711c93aa969fd5cef7a3d7f
Filip Koed Nymann <87913095+FleshIsBack@users.noreply.github.com>
2023-11-21 12:42:48 +0100

    Functionallity added + simple fieldChecks

--- Client/Forms/CustomerPanels/CreateCustomer.cs
+++ Client/Forms/CustomerPanels/CreateCustomer.cs
@@ -1,4 +1,6 @@
-﻿using Models;
+﻿using Client.Controllers;
+using Client.DAL;
+using Models;
 
 namespace Client.Forms.CustomerPanels;
 
@@ -11,7 +13,79 @@ public partial class CreateCustomer : Form
 
     private void btnCreate_Click(object sender, EventArgs e)
     {
-        DialogResult = DialogResult.OK;
+        // Get customer and address from the form
+        var customer = this.Customer;
+        var address = this.Address;
+
+        if (!IsCustomerValid(customer) || !IsAddressValid(address) ||
+            !IsPasswordValid(customer.Password, tbPasswordConfirm.Text) ||
+            !IsEmailValid(customer.Email))
+        {
+            MessageBox.Show("Please check your input. Ensure all fields are correctly filled.");
+            return;
+        }
+
+
+        // Create instances of CustomerController and AddressDA
+        var customerController = new CustomerController();
+        var AddressController = new AddressController();
+
+        // First, create the address
+        var createdAddress = AddressController.Create(address);
+        if (createdAddress != null && createdAddress.ID.HasValue)
+        {
+            // Set the AddressID of the customer
+            customer.AddressID = createdAddress.ID;
+
+            // Create the customer
+            var createdCustomer = customerController.Create(customer);
+            if (createdCustomer != null)
+            {
+                MessageBox.Show("Customer and Address created successfully!");
+                return;
+            }
+        }
+
+        MessageBox.Show("Failed to create Customer and Address.");
+        DialogResult = DialogResult.Cancel;
+    }
+
+    private bool IsCustomerValid(Customer customer)
+    {
+        // Add validation logic for Customer
+        return !string.IsNullOrWhiteSpace(customer.FirstName) &&
+               !string.IsNullOrWhiteSpace(customer.LastName) &&
+               !string.IsNullOrWhiteSpace(customer.Email) &&
+               !string.IsNullOrWhiteSpace(customer.PhoneNo) &&
+               !string.IsNullOrWhiteSpace(customer.Password);
+    }
+
+    private bool IsAddressValid(Address address)
+    {
+        // Add validation logic for Address
+        return !string.IsNullOrWhiteSpace(address.Street) &&
+               !string.IsNullOrWhiteSpace(address.City) &&
+               !string.IsNullOrWhiteSpace(address.Zip) &&
+               !string.IsNullOrWhiteSpace(address.HouseNumber);
+    }
+    private bool IsPasswordValid(string password, string confirmPassword)
+    {
+        // Check if the password is not empty and matches the confirm password
+        return !string.IsNullOrWhiteSpace(password) && password == confirmPassword;
+    }
+
+    private bool IsEmailValid(string email)
+    {
+        // Simple email validation logic
+        try
+        {
+            var Eaddr = new System.Net.Mail.MailAddress(email);
+            return Eaddr.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
     }
 
     private void btnCancel_Click(object sender, EventArgs e)
